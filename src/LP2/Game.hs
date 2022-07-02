da338f3f75b5050c38460d8cb1383c16712e10de
André Dias Nunes <andredias@ice.ufjf.br>
2022-07-01 22:42:25 -0300

    v1.0 - Completos

--- src/LP2/Game.hs
+++ src/LP2/Game.hs
@@ -11,17 +11,63 @@ module Game where
 import Data.IORef
 
 gameLoop :: IORef Integer -> String -> IO ()
-gameLoop turn secret = do
+gameLoop turn secret = do                                              
+    hits <- newIORef 0                                                  -- Inicializa contador de acertos
+    partial <- newIORef 0                                               -- Inicializa contador de parciais                                    
     putStr "? "
-    input <- getLine
-    if (length input /= 7)
-        then putStrLn "Entrada inválida." >> gameLoop turn secret
+    input <- getLine                                                    -- Entrada
+    valid <- validate (input)                                           -- Verifica entrada
+    if (valid)
+        then do
+            putStrLn "Entrada inválida."
+            putStrLn "Sendo \"n\" um número entre 1 e 6. Insira no formato: n n n n" >> gameLoop turn secret        -- Loop
         else do
-            modifyIORef turn (+1)
+            modifyIORef' turn (+1)
             if (input == secret)
                 then do
                     putStr ("Parabéns, você acertou após ")
                     readIORef turn >>= print
                     -- putStrLn (" tentativas")  -- ?? Como printar na msm linha
-                else putStrLn "Nope. Guess again." >> gameLoop turn secret
+                else do
+                    check secret input hits partial
+                    putStr "Completos: "
+                    readIORef hits >>= print
+                    gameLoop turn secret
+                    -- putStr "Parciais: " >> gameLoop turn secret
+                    -- readIORef partial >>= print
 
+-- Função para iniciar processo de encontrar acertos e parciais
+check :: String -> String -> IORef Integer -> IORef Integer -> IO ()
+check secret input hits partial = do
+    open1 <- newIORef True
+    open2 <- newIORef True
+    open3 <- newIORef True
+    open4 <- newIORef True
+    hitPosition 0 secret input hits open1
+    hitPosition 2 secret input hits open2
+    hitPosition 4 secret input hits open3
+    hitPosition 6 secret input hits open4
+ 
+-- Função para encontrar acertos
+hitPosition :: Int -> String -> String -> IORef Integer -> IORef Bool -> IO ()
+hitPosition pos secret input hits open = do
+    if(input !! pos == secret !! pos)                       -- Se input[pos] == secret[pos]
+        then do 
+            writeIORef open False                           -- Muda estado do digito para fechado
+            modifyIORef hits (+1)                           -- Incrementa o hits
+        else 
+            return ()
+
+-- partialPosition :: String -> String -> IORef Integer -> IORef Bool -> IORef Bool -> IORef Bool -> IORef Bool -> IO ()
+-- partialPosition secret input partial open1 open2 open3 open4 = do
+    
+
+validate :: String -> IO (Bool)
+validate input = do
+    if(length input /= 7)
+        then return True
+        else return False
+
+
+-- then if(input !! 0 >= 1 && input !! 0 <= 6)
+-- validateRange :: 
--- src/LP2/Main.hs
+++ src/LP2/Main.hs
@@ -16,21 +16,22 @@ import Data.IORef
 
 main :: IO ()
 main = do 
+    menu
+    listSecret <- randomList (4)                                -- Gera lista de [1..6]
+    let secret = listToString listSecret                        -- Converte para string
+    print (secret)                                              -- Segredo
+    turn <- newIORef 0                                          -- Inicializa contador de turnos       
+    gameLoop turn secret                                        -- Inicia o jogo
+    
+
+
+menu = do
     putStrLn "\n|------------|"
     putStrLn "| CriptoGame |"
     putStrLn "|------------|\n"
-    listSecret <- randomList (4)
-    let secret = listToString listSecret
-    print (secret)
     putStrLn "Regras:"
     putStrLn " - Insira uma sequencia de 4 números"
     putStrLn " - Cada números separado por espaço"
     putStrLn " - Números dentro do intervalod de 1 a 6"
-    turn <- newIORef 0       
-    gameLoop turn secret
-    
-
-
-
 
 
