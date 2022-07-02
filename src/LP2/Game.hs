87dfc12c529724fc51d681d41a8aaaf6f0d85710
André Dias Nunes <andredias@ice.ufjf.br>
2022-07-01 23:04:57 -0300

    v1.0.5 - Cleaning

--- src/LP2/Game.hs
+++ src/LP2/Game.hs
@@ -7,7 +7,6 @@
 -- Matrícula: 201765551AC
 
 module Game where
---import Utilities (increment)
 import Data.IORef
 
 gameLoop :: IORef Integer -> String -> IO ()
@@ -22,28 +21,28 @@ gameLoop turn secret = do
             putStrLn "Entrada inválida."
             putStrLn "Sendo \"n\" um número entre 1 e 6. Insira no formato: n n n n" >> gameLoop turn secret        -- Loop
         else do
-            modifyIORef' turn (+1)
-            if (input == secret)
+            modifyIORef' turn (+1)                                      -- Incrementa o turno
+            if (input == secret)                                        -- Se entrada == segredo
                 then do
                     putStr ("Parabéns, você acertou após ")
                     readIORef turn >>= print
                     -- putStrLn (" tentativas")  -- ?? Como printar na msm linha
                 else do
-                    check secret input hits partial
+                    check secret input hits partial                     -- Verifica acertos/parciais 
                     putStr "Completos: "
                     readIORef hits >>= print
-                    gameLoop turn secret
+                    gameLoop turn secret                                -- Proximo turno
                     -- putStr "Parciais: " >> gameLoop turn secret
                     -- readIORef partial >>= print
 
 -- Função para iniciar processo de encontrar acertos e parciais
 check :: String -> String -> IORef Integer -> IORef Integer -> IO ()
 check secret input hits partial = do
-    open1 <- newIORef True
+    open1 <- newIORef True                      -- Inicializa estado "aberto" da posição
     open2 <- newIORef True
     open3 <- newIORef True
     open4 <- newIORef True
-    hitPosition 0 secret input hits open1
+    hitPosition 0 secret input hits open1       -- Verifica se acertou valor da posição
     hitPosition 2 secret input hits open2
     hitPosition 4 secret input hits open3
     hitPosition 6 secret input hits open4
@@ -53,15 +52,14 @@ hitPosition :: Int -> String -> String -> IORef Integer -> IORef Bool -> IO ()
 hitPosition pos secret input hits open = do
     if(input !! pos == secret !! pos)                       -- Se input[pos] == secret[pos]
         then do 
-            writeIORef open False                           -- Muda estado do digito para fechado
+            writeIORef open False                           -- Muda estado da posição para fechado
             modifyIORef hits (+1)                           -- Incrementa o hits
-        else 
-            return ()
+        else return ()
 
 -- partialPosition :: String -> String -> IORef Integer -> IORef Bool -> IORef Bool -> IORef Bool -> IORef Bool -> IO ()
 -- partialPosition secret input partial open1 open2 open3 open4 = do
     
-
+-- Função para validar entrada
 validate :: String -> IO (Bool)
 validate input = do
     if(length input /= 7)
--- src/LP2/Main.hs
+++ src/LP2/Main.hs
@@ -8,30 +8,39 @@
 
 module Main where
 
-import Utilities (randomList, listToString)
-import Game (gameLoop)
 import Data.IORef
+import System.Random (randomRIO)
 
-
+import Game (gameLoop)
 
 main :: IO ()
 main = do 
     menu
     listSecret <- randomList (4)                                -- Gera lista de [1..6]
     let secret = listToString listSecret                        -- Converte para string
-    print (secret)                                              -- Segredo
+    putStrLn ("Segredo: " ++ show secret ++ "\n")               -- Segredo
     turn <- newIORef 0                                          -- Inicializa contador de turnos       
     gameLoop turn secret                                        -- Inicia o jogo
     
-
-
 menu = do
-    putStrLn "\n|------------|"
+    putStrLn "\n|~~~~~~~~~~~~|"
     putStrLn "| CriptoGame |"
-    putStrLn "|------------|\n"
+    putStrLn "|~~~~~~~~~~~~|\n"
     putStrLn "Regras:"
     putStrLn " - Insira uma sequencia de 4 números"
     putStrLn " - Cada números separado por espaço"
     putStrLn " - Números dentro do intervalod de 1 a 6"
 
+-- Gera lista de números aleatórios
+randomList :: Int -> IO([Int])
+randomList 0 = return []
+randomList n = do
+    r  <- randomRIO (1,6)
+    rs <- randomList (n-1)
+    return (r:rs)
 
+-- Transforma lista para string
+listToString :: (Show a) => [a] -> String
+listToString [] = ""
+listToString [x] = (show x)
+listToString (x:xs) = (show x) ++ " " ++ listToString xs
